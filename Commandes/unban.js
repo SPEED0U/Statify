8fc4752d0120f0e61988bfb60909cd5a38031360
Steven M <speedou.tm.prod@gmail.com>
2021-10-08 00:09:18 +0200

    Refactored code and added compatibility for new settings

--- Commandes/unban.js
+++ Commandes/unban.js
@@ -9,6 +9,7 @@ module.exports.run = (bot, message, args, con) => {
             } else {
                 if (result.length > 0) {
                     var userid = result[0].USERID;
+                    var icon = result[0].iconIndex + settings.url.avatarFormat
                     var reason = message.content.replace("s!unban", '').replace(args[0], '').trim();
                     con.query("SELECT * FROM BAN WHERE user_id = " + userid + " AND active = 1", (err, result1) =>
                         con.query("SELECT * FROM HARDWARE_INFO WHERE userId = " + userid + " AND banned = 1", (err, result2) => {
@@ -16,19 +17,25 @@ module.exports.run = (bot, message, args, con) => {
                                 con.query("UPDATE BAN SET active = 0 WHERE user_id = " + userid); {
                                     con.query("UPDATE HARDWARE_INFO SET banned = 0 WHERE userId = " + userid)
                                     if (reason.length > 0) {
-                                        if (!err) message.channel.send("Unbanned **" + args[0] + "** with reason: `" + reason + "`."); {
-                                            const embed = new MessageEmbed()
-                                                .setColor("#2fff00")
-                                                .setDescription("**" + args[0] + "** has been unbanned.\nReason: `" + reason + "`")
-                                            bot.channels.cache.get(settings.channel.banlogs).send({embeds:[embed]})
-                                        }
+                                        const embed = new MessageEmbed()
+                                        .setAuthor(args[0].toUpperCase() + " has been unbanned.", settings.url.avatarEndpoint + icon)
+                                        .setColor("#11ff00")
+                                        .addField("Reason", reason)
+                                        .addField("Unbanned by", message.author.tag)
+                                        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                                        .setTimestamp()
+                                    bot.channels.cache.get(settings.channel.banlogs).send({embeds:[embed]})
+                                    message.channel.send({embeds:[embed]})
                                     } else {
-                                        if (!err) message.channel.send("Unbanned **" + args[0] + "**."); {
-                                            const embed = new MessageEmbed()
-                                                .setColor("#2fff00")
-                                                .setDescription("**" + args[0] + "** has been unbanned.")
-                                            bot.channels.cache.get(settings.channel.banlogs).send({embeds:[embed]})
-                                        }
+                                        const embed = new MessageEmbed()
+                                        .setAuthor(args[0].toUpperCase() + " has been unbanned.", settings.url.avatarEndpoint + icon)
+                                        .setColor("#11ff00")
+                                        .addField("Reason", "No reason specified.")
+                                        .addField("Unbanned by", message.author.tag)
+                                        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                                        .setTimestamp()
+                                        bot.channels.cache.get(settings.channel.banlogs).send({embeds:[embed]})
+                                        message.channel.send({embeds:[embed]})
                                     }
                                 }
                             }
