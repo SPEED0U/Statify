cf54f8a2207070cad919c41a780ac4afbbcf7bfb
Steven M <speedou.tm.prod@gmail.com>
2021-10-08 00:05:09 +0200

    Reworked embed of rename command output

--- Commandes/rename.js
+++ Commandes/rename.js
@@ -4,11 +4,21 @@ module.exports.run = (bot, message, args, con) => {
         con.query("SELECT ID, USERID FROM PERSONA WHERE name = ?", [args[0]], (err, pid) => {
             if (pid.length > 0) {
                 let personaid = pid[0].ID
+                var icon = result[0].iconIndex + settings.url.avatarFormat
                 if (args[1] != undefined) {
                     con.query("SELECT ID FROM PERSONA WHERE name = ?", [args[1]], (err, result1) => {
                         if (result1.length === 0) {
                             con.query("UPDATE PERSONA SET name = ? WHERE ID = " + personaid, [args[1].toUpperCase()], err => {
-                                if (!err) message.channel.send("Renamed **" + args[0] + "** driver name to **" + args[1] + "**.")
+                                if (!err)
+                                    const embed = new MessageEmbed()
+                                        .setAuthor(args[0].toUpperCase() + " has been renamed.", settings.url.avatarEndpoint + icon)
+                                        .setColor("#0080ff")
+                                        .addField("Renamed to", personaid)
+                                        .addField("Renamed by", message.author.tag)
+                                        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                                        .setTimestamp()
+                                bot.channels.cache.get(settings.channel.banlogs).send({ embeds: [embed] })
+                                message.channel.send({ embeds: [embed] })
                             })
                         } else {
                             message.channel.send("There is already a **driver** called **" + args[1].toUpperCase() + "**.")
@@ -16,8 +26,17 @@ module.exports.run = (bot, message, args, con) => {
                     })
                 } else {
                     con.query("UPDATE PERSONA SET name = 'DRIVER" + personaid + "' WHERE ID = " + personaid, (err, result) => {
-                        if (!err) message.channel.send("Renamed **" + args[0] + "** driver name to **DRIVER" + personaid + "**.")
-                        else {
+                        if (!err) {
+                            const embed = new MessageEmbed()
+                                .setAuthor(args[0].toUpperCase() + " has been renamed.", settings.url.avatarEndpoint + icon)
+                                .setColor("#0080ff")
+                                .addField("Renamed to", "DRIVER" + personaid)
+                                .addField("Renamed by", message.author.tag)
+                                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                                .setTimestamp()
+                            bot.channels.cache.get(settings.channel.banlogs).send({ embeds: [embed] })
+                            message.channel.send({ embeds: [embed] })
+                        } else {
                             con.query("UPDATE PERSONA SET name = 'DRIVER" + (personaid + 1) + "' WHERE ID = " + personaid, (err, result) => {
                                 if (!err) message.channel.send("Renamed **" + args[0] + "** driver name to **DRIVER" + (personaid + 1) + "**.")
                             })
