eb7f1e6c60531c3e368dc8d78ca07caf41e6df60
Steven M <34949863+SPEED0U@users.noreply.github.com>
2021-10-27 13:25:40 +0200

    Updated settings

--- settings.js
+++ settings.js
@@ -1,3 +1,32 @@
+const fs = require("fs");
+const mergeJSON = function (target, add) {
+    function isObject(obj) {
+        if (typeof obj == "object") {
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    return true; // search for first object prop
+                }
+            }
+        }
+        return false;
+    }
+    for (var key in add) {
+        if (add.hasOwnProperty(key)) {
+            if (target[key] && isObject(target[key]) && isObject(add[key])) {
+                mergeJSON(target[key], add[key]);
+            } else {
+                target[key] = add[key];
+            }
+        }
+    }
+    return target;
+};
+
+if (!fs.existsSync("./local.settings.json")) {
+    fs.writeFileSync("./local.settings.json", fs.readFileSync("./settings.json"))
+    console.log('The file "local.settings.json" has been created, you must fill the parameters.\nSee the documentation on Github for more informations.')
+    process.exit(0)
+}
 const settings = require("./settings.json")
 const localSettings = require("./local.settings.json")
-module.exports = {...settings,...localSettings}
+module.exports = mergeJSON(settings,localSettings)
