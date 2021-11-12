63cb85ecea13f2aa8a44f460013dab91bd8a276d
Steven M <34949863+SPEED0U@users.noreply.github.com>
2021-11-12 13:32:24 +0100

    Unban command rework + fixes

--- Commands/unban.js
+++ Commands/unban.js
@@ -12,11 +12,11 @@ module.exports.run = (bot, message, args, con) => {
                     var icon = result[0].iconIndex + settings.url.avatarFormat
                     var reason = message.content.replace("s!unban", '').replace(args[0], '').trim();
                     con.query("SELECT gameHardwareHash AS ghh FROM USER WHERE ID = ?", [userid], (err, userInfo) =>
-                    con.query("SELECT * FROM BAN WHERE user_id = " + userid + " AND active = 1", (err, result1) =>
-                    con.query("UPDATE HARDWARE_INFO SET banned = 1 WHERE userId = ? AND hardwareHash = ?", [userid,userInfo[0].ghh], (err, result2) => {
+                    con.query("SELECT * FROM BAN WHERE user_id = ? AND active = 1", [userid], (err, result1) =>
+                    con.query("SELECT * FROM HARDWARE_INFO WHERE userId = ? AND hardwareHash = ? AND banned = 1", [userid,userInfo[0].ghh], (err, result2) => {
                             if (result1.length > 0 || result2.length > 0) {
-                                con.query("UPDATE BAN SET active = 0 WHERE user_id = " + userid); {
-                                    con.query("UPDATE HARDWARE_INFO SET banned = 0 WHERE userId = " + userid + " AND hardwareHash = '" + userInfo[0].ghh + "'")
+                                con.query("UPDATE BAN SET active = 0 WHERE user_id = ?", [userid]); {
+                                    con.query("UPDATE HARDWARE_INFO SET banned = 0 WHERE userId = ? AND hardwareHash = ?", [userid,userInfo[0].ghh])
                                     if (reason.length > 0) {
                                         const embed = new MessageEmbed()
                                         .setAuthor(result[0].name + " has been unbanned.", settings.url.avatarEndpoint + icon)
