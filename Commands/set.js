1c48240273ed537e5b40418e929da610a4d73c71
Steven M <34949863+SPEED0U@users.noreply.github.com>
2021-11-29 17:39:46 +0100

    Set command and output rework

--- Commands/set.js
+++ Commands/set.js
@@ -1,5 +1,5 @@
 const settings = require("../settings.js");
-const axios = require('axios');
+const { MessageEmbed } = require('discord.js');
 const commands = {
     repmultiplier,
     cashmultiplier,
@@ -14,38 +14,74 @@ module.exports.run = (bot, message, args, con) => {
             commands[args[0]](bot, message, args, con)
         } else {
             const embed = new MessageEmbed()
-            .setColor("#ff0000")
-            .addField("Insufficient permissions", "You need `" + this.help.category.substring(4) + "` permissions to run this command.")
-            .setFooter(bot.user.tag, bot.user.displayAvatarURL())
-            .setTimestamp()
-        message.channel.send({ embeds: [embed] })
+                .setColor("#ff0000")
+                .addField("Insufficient permissions", "You need `" + this.help.category.substring(4) + "` permissions to run this command.")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
         }
     }
 };
 
-
 function repmultiplier(bot, message, args, con) {
     con.query("UPDATE PARAMETER SET value = ? WHERE name = 'REP_REWARD_MULTIPLIER'", [args[1]], err => {
-        if (!err) message.channel.send("<:rep:814916853318025227> • Updated **rep multiplier** to **X" + parseInt(args[1]) + "**\n" + "Apply the changes by using `" + settings.bot.prefix + "reload`.")
+        if (!err) {
+            const embed = new MessageEmbed()
+                .setAuthor("Reward multiplier")
+                .setColor("#0090e3")
+                .addField("Reputation", "The **rep** multiplier is now `X" + args[1] + "`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+            newSettings(bot, message, args, con)
+        }
     })
 }
 
 function cashmultiplier(bot, message, args, con) {
     con.query("UPDATE PARAMETER SET value = ? WHERE name = 'CASH_REWARD_MULTIPLIER'", [args[1]], err => {
-        if (!err) message.channel.send("<:cash:814916853595766784> • Updated **cash multiplier** to **X" + parseInt(args[1]) + "**\n" + "Apply the changes by using `" + settings.bot.prefix + "reload`.")
+        if (!err) {
+            const embed = new MessageEmbed()
+                .setAuthor("Reward multiplier")
+                .setColor("#b2c6d1")
+                .addField("Cash", "The **cash** multiplier is now `X" + args[1] + "`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+            newSettings(bot, message, args, con)
+        }
     })
 }
 
 function multiplier(bot, message, args, con) {
     con.query("UPDATE PARAMETER SET value = ? WHERE name IN('CASH_REWARD_MULTIPLIER', 'REP_REWARD_MULTIPLIER')", [args[1]], err => {
-        if (!err) message.channel.send("<:cash:814916853595766784> <:rep:814916853318025227> • Updated **cash & rep multiplier** to **X" + parseInt(args[1]) + "**\n" + "Apply the changes by using `" + settings.bot.prefix + "reload`.")
+        if (!err) {
+            const embed = new MessageEmbed()
+                .setAuthor("Reward multiplier")
+                .setColor("#75dfff")
+                .addField("Cash", "The **cash** multiplier is now `X" + args[1] + "`")
+                .addField("Reputation", "The **rep** multiplier is now `X" + args[1] + "`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+            newSettings(bot, message, args, con)
+        }
     })
 }
 
 function modsversion(bot, message, args, con) {
     const modsurl = settings.url.modsEndpoint + args[1]
     con.query("UPDATE PARAMETER SET value = ? WHERE name = 'MODDING_BASE_PATH'", [modsurl], err => {
-        if (!err) message.channel.send(":gear: • Updated **mods version** to **" + args[1] + "**\n" + "Apply the changes by using `" + settings.bot.prefix + "reload`.")
+        if (!err) {
+            const embed = new MessageEmbed()
+                .setAuthor("Server mods")
+                .setColor("#00ff84")
+                .addField("Version", "The **mods version** is now `" + args[1] + "`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+            newSettings(bot, message, args, con)
+        }
     })
 }
 
@@ -54,40 +90,76 @@ function scenery(bot, message, args, con) {
         case "fireworks":
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_NEWYEARS' WHERE name = 'SERVER_INFO_ENABLED_SCENERY'")
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_NEWYEARS_DISABLED' WHERE name = 'SERVER_INFO_DISABLED_SCENERY'")
-            axios.post(settings.core.url + '/Engine.svc/ReloadParameters', "adminAuth=" + settings.core.token.server, null)
-            message.channel.send(":fireworks: • Enabled **" + args[1] + "** scenery.")
+            var embed = new MessageEmbed()
+                .setAuthor("Game scenery")
+                .setColor("#ffffff")
+                .addField("Scenery", "The **scenery** has been set to `fireworks`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
             break;
         case "normal":
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_NORMAL' WHERE name = 'SERVER_INFO_ENABLED_SCENERY'")
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_NORMAL_DISABLED' WHERE name = 'SERVER_INFO_DISABLED_SCENERY'")
-            axios.post(settings.core.url + '/Engine.svc/ReloadParameters', "adminAuth=" + settings.core.token.server, null)
-            message.channel.send(":cityscape: • Enabled **" + args[1] + "** scenery.")
+            var embed = new MessageEmbed()
+                .setAuthor("Game scenery")
+                .setColor("#ffffff")
+                .addField("Scenery", "The **scenery** has been set to `normal`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
             break;
         case "halloween":
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_HALLOWEEN' WHERE name = 'SERVER_INFO_ENABLED_SCENERY'")
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_HALLOWEEN_DISABLED' WHERE name = 'SERVER_INFO_DISABLED_SCENERY'")
-            axios.post(settings.core.url + '/Engine.svc/ReloadParameters', "adminAuth=" + settings.core.token.server, null)
-            message.channel.send(":ghost: • Enabled **" + args[1] + "** scenery.")
+            var embed = new MessageEmbed()
+                .setAuthor("Game scenery")
+                .setColor("#ffffff")
+                .addField("Scenery", "The **scenery** has been set to `halloween`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
             break;
         case "christmas":
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_CHRISTMAS' WHERE name = 'SERVER_INFO_ENABLED_SCENERY'")
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_CHRISTMAS_DISABLED' WHERE name = 'SERVER_INFO_DISABLED_SCENERY'")
-            axios.post(settings.core.url + '/Engine.svc/ReloadParameters', "adminAuth=" + settings.core.token.server, null)
-            message.channel.send(":christmas_tree: • Enabled **" + args[1] + "** scenery.")
+            var embed = new MessageEmbed()
+                .setAuthor("Game scenery")
+                .setColor("#ffffff")
+                .addField("Scenery", "The **scenery** has been set to `christmas`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
             break;
         case "oktoberfest":
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_OKTOBERFEST' WHERE name = 'SERVER_INFO_ENABLED_SCENERY'")
             con.query("UPDATE PARAMETER SET value = 'SCENERY_GROUP_OKTOBERFEST_DISABLED' WHERE name = 'SERVER_INFO_DISABLED_SCENERY'")
-            axios.post(settings.core.url + '/Engine.svc/ReloadParameters', "adminAuth=" + settings.core.token.server, null)
-            message.channel.send(":beers: • Enabled **" + args[1] + "** scenery.")
+            var embed = new MessageEmbed()
+                .setAuthor("Game scenery")
+                .setColor("#ffffff")
+                .addField("Scenery", "The **scenery** has been set to `oktoberfest`")
+                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
             break;
     }
+    newSettings(bot, message, args, con)
 }
+
+function newSettings(bot, message, args, con) {
+    const embed = new MessageEmbed()
+        .setColor("#ff7700")
+        .addField("Settings changed", "To apply the new settings use `" + settings.bot.prefix + "reload`.")
+        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+        .setTimestamp()
+    message.channel.send({ embeds: [embed] })
+}
+
 module.exports.help = {
     name: "set",
-    description: ["Changes the global multiplier value.","Changes the reputation multiplier value.","Changes the cash multiplier value.","Changes the version of game mods.","Changes the game map scenery."],
-    param: ["multiplier","repmultiplier","cashmultiplier","modsversion","scenery"],
+    description: ["Changes the global multiplier value.", "Changes the reputation multiplier value.", "Changes the cash multiplier value.", "Changes the version of game mods.", "Changes the game map scenery."],
+    param: ["multiplier", "repmultiplier", "cashmultiplier", "modsversion", "scenery"],
     category: "[⚔️] Moderator",
     args: "[player] [reason]",
-    roles: [settings.role.admin,settings.role.moderator] 
+    roles: [settings.role.admin, settings.role.moderator]
 };
