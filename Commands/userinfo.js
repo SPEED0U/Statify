d559b8dd5619708ba6563ad7c2c5c5c285fc2fa2
Steven M <34949863+SPEED0U@users.noreply.github.com>
2021-12-03 18:35:21 +0100

    Fixed a crash when the requested driver wasn't found + added email support

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -2,50 +2,54 @@ const settings = require("../settings.js");
 const { MessageEmbed } = require('discord.js');
 module.exports.run = (bot, message, args, con) => {
     if ((message.channel.id === settings.channel.command.admin || message.channel.id === settings.channel.command.moderator) && (message.member && message.member.roles.cache.find(r => r.id === settings.role.moderator))) {
-        con.query("SELECT ID, USERID, iconIndex FROM PERSONA WHERE name = ?", [args[0]], (err, pid) => {
-            con.query("SELECT EMAIL, gameHardwareHash, IP_ADDRESS, lastLogin, isLocked, created, ID, premium FROM USER WHERE id = ?", [pid[0].USERID], (err, uid) => {
-                var email = uid[0].EMAIL
-                var ghh = uid[0].gameHardwareHash
-                var ip = uid[0].IP_ADDRESS
-                var lastlog = uid[0].lastLogin
-                var locked = uid[0].isLocked.readInt8()
-                var accCreation = uid[0].created
-                var icon = pid[0].iconIndex + settings.url.avatarFormat
-                var userId = uid[0].ID
-                var premium = uid[0].premium.readInt8()
-                con.query("SELECT name FROM PERSONA WHERE USERID = ?", [userId], (err, drivers) => {
-                    var attachedDrivers = []
-                    for (driver of drivers) {
-                        attachedDrivers.push(driver.name)
-                    }
-                    if (pid.length > 0) {
-                        const embed = new MessageEmbed()
-                            .setAuthor("Account information of " + args[0].toUpperCase(), settings.url.avatarEndpoint + icon)
-                            .setThumbnail(settings.url.avatarEndpoint + icon, true)
-                            .setColor("#ff0000")
-                            .addField("Email", "`" + email + "`")
-                            .addField("Account ID", "`" + userId + "`")
-                            .addField("Account creation date", "`" + accCreation.toLocaleString('en-GB', { timeZone: "Europe/Paris", hour12: false }) + "`")
-                            .addField("Last connection", "`" + lastlog.toLocaleString('en-GB', { timeZone: "Europe/Paris", hour12: false }) + "`")
-                            .addField("Membership", premium == 1 ? "`Premium`" : "`Freemium`")
-                            .addField("Attached drivers", "`" + attachedDrivers.join(", ") + "`")
-                            .addField("Hardware hash", "`" + ghh.toUpperCase() + "`")
-                            .addField("IP address", "`" + ip + "`")
-                            .addField("Account state", locked == 1 ? "`Locked`" : "`Unlocked`")
-                            .setFooter(bot.user.tag, bot.user.displayAvatarURL())
-                            .setTimestamp()
-                        message.channel.send({ embeds: [embed] })
-                    } else message.channel.send("Driver **" + args[0] + "** not found.")
-                })
+        con.query("SELECT ID FROM USER WHERE email = ?", [args[0]], (err, userid) => {
+            con.query("SELECT ID, USERID, iconIndex FROM PERSONA WHERE name = ?", [args[0]], (err, pid) => {
+                if (pid.length > 0 || userid.length > 0) {
+                    con.query("SELECT EMAIL, gameHardwareHash, IP_ADDRESS, lastLogin, isLocked, created, ID, premium FROM USER WHERE id = ?", [pid.length > 0 ? pid[0].USERID : userid[0].ID], (err, uid) => {
+                        var email = uid[0].EMAIL
+                        var ghh = uid[0].gameHardwareHash
+                        var ip = uid[0].IP_ADDRESS
+                        var lastlog = uid[0].lastLogin
+                        var locked = uid[0].isLocked.readInt8()
+                        var accCreation = uid[0].created
+                        var userId = uid[0].ID
+                        var premium = uid[0].premium.readInt8()
+                        con.query("SELECT name FROM PERSONA WHERE USERID = ?", [userId], (err, drivers) => {
+                            var attachedDrivers = []
+                            for (driver of drivers) {
+                                attachedDrivers.push(driver.name)
+                            }
+                            const embed = new MessageEmbed()
+                                .setAuthor("Account information of " + args[0].toUpperCase())
+                                .setColor("#ff0000")
+                                .addField("Email", "`" + email + "`")
+                                .addField("Account ID", "`" + userId + "`")
+                                .addField("Account creation date", "`" + accCreation.toLocaleString('en-GB', { timeZone: "Europe/Paris", hour12: false }) + "`")
+                                .addField("Last connection", "`" + lastlog.toLocaleString('en-GB', { timeZone: "Europe/Paris", hour12: false }) + "`")
+                                .addField("Membership", premium == 1 ? "`Premium`" : "`Freemium`")
+                                .addField("Attached drivers", "`" + attachedDrivers.join(", ") + "`")
+                                .addField("Hardware hash", "`" + ghh.toUpperCase() + "`")
+                                .addField("IP address", "`" + ip + "`")
+                                .addField("Account state", locked == 1 ? "`Locked`" : "`Unlocked`")
+                                .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+                                .setTimestamp()
+                            message.channel.send({ embeds: [embed] })
+                        })
+                    })
+                } else if (args[0].includes('@')) {
+                    message.channel.send("Email **" + args[0] + "** not found.")
+                } else {
+                    message.channel.send("Driver **" + args[0] + "** not found.")
+                }
             })
         })
     } else {
         const embed = new MessageEmbed()
-        .setColor("#ff0000")
-        .addField("Insufficient permissions", "You need `" + this.help.category.substring(4) + "` permissions to run this command.")
-        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
-        .setTimestamp()
-    message.channel.send({ embeds: [embed] })
+            .setColor("#ff0000")
+            .addField("Insufficient permissions", "You need `" + this.help.category.substring(4) + "` permissions to run this command.")
+            .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+            .setTimestamp()
+        message.channel.send({ embeds: [embed] })
     }
 };
 
