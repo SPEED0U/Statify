ff8dfaa7b2cd74123c9df5807fd3ba8bb06818ba
Steven M <speedou.tm.prod@gmail.com>
2021-12-23 12:35:52 +0100

    Added a MySQL disconnection manager

--- index.js
+++ index.js
@@ -6,27 +6,48 @@ const mysql = require('mysql');
 const settings = require("./settings.js");
 var con = mysql.createConnection(settings.sql);
 
+function handleDisconnect() {
+    con = mysql.createConnection(settings.sql);
+
+    con.connect(function (err) {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    con.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
 bot.login(settings.bot.token)
-bot.on("ready", function() {
+bot.on("ready", function () {
     fs.readdir('./Events/', (error, f) => {
         if (error) { return console.error(error); }
-        
+
         f.forEach((f) => {
             let events = require(`./Events/${f}`);
             let event = f.split('.')[0];
             bot.on(event, events.bind(null, bot, con));
             console.log("Loaded " + f + " event.");
-            });
-        })
+        });
+    })
     fs.readdir('./Scripts/', (error, f) => {
-            if (error) { return console.error(error); }
-            console.log("Loaded " + f + " script.");
+        if (error) { return console.error(error); }
+        console.log("Loaded " + f + " script.");
 
-            f.forEach((f) => {
-                let events = require(`./Scripts/${f}`);
-                events(bot, con);
-            });
-        })
+        f.forEach((f) => {
+            let events = require(`./Scripts/${f}`);
+            events(bot, con);
+        });
+    })
     fs.readdir("./Commands/", (error, f) => {
         if (error) { return console.error(error); }
         let commands = f.filter(file => file.endsWith(".js"));
@@ -40,8 +61,8 @@ bot.on("ready", function() {
     });
 })
 
-if (!Array.prototype.last){
-    Array.prototype.last = function(){
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
         return this[this.length - 1];
     };
 };
