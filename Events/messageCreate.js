21f64d77e687df3c1ca9647d0a89d61021edb096
Steven M <speedou.tm.prod@gmail.com>
2022-08-01 11:20:54 +0200

    Update messageCreate.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -34,92 +34,89 @@ module.exports = (bot, con, message) => {
 }
 
 function antispamscam(message, bot) {
-    try {
-        if (message.content.length > 0 && message.content.includes('http' || '/' || '.' || ':')) {
-            if (users[message.author.id]) {
-                const user = users[message.author.id]
-                const lastMessage = user.messages.last()
-                const diff = (message.createdTimestamp - lastMessage.createdTimestamp) / 1000
-                if (diff <= settings.antiscam.delay && lastMessage.content === message.content) {
-                    if (!user.messages.includes(lastMessage)) user.message.push(lastMessage)
-                    user.messages.push(message)
-                    const count = user.messages.length
-                    if (count === settings.antiscam.maxDuplicate) {
-                        for (const msg of user.messages) {
-                            msg.delete()
-                        }
-                        message.member.kick('Kicked for spamming "' + message.content + '"')
-                        const embed = new MessageEmbed()
-                            .setAuthor({
-                                name: message.author.tag + " has been kicked.",
-                                iconURL: message.author.displayAvatarURL()
-                            })
-                            .setColor("#ff0000")
-                            .addField("Spamming the following text", "```" + message.content + "```")
-                            .addField("Discord user id", "`" + message.author.id + "`")
-                            .setFooter({
-                                text: bot.user.tag,
-                                iconURL: bot.user.displayAvatarURL()
-                            })
-                            .setTimestamp()
-                        bot.channels.cache.get(settings.channel.serverlogs).send({ embeds: [embed] })
+    if (message.content.length > 0 && message.content.includes('http' || '/' || '.' || ':')) {
+        if (users[message.author.id]) {
+            const user = users[message.author.id]
+            const lastMessage = user.messages.last()
+            const diff = (message.createdTimestamp - lastMessage.createdTimestamp) / 1000
+            if (diff <= settings.antiscam.delay && lastMessage.content === message.content) {
+                if (!user.messages.includes(lastMessage)) user.message.push(lastMessage)
+                user.messages.push(message)
+                const count = user.messages.length
+                if (count === settings.antiscam.maxDuplicate) {
+                    for (const msg of user.messages) {
                         try {
-                            delete users[message.author.id]
+                            msg.delete()
                         } catch (error) {
                             console.log("The message that should've been deleted by myself has already been deleted.")
                         }
                     }
-                } else {
-                    user.messages = [message]
+                    message.member.kick('Kicked for spamming "' + message.content + '"')
+                    const embed = new MessageEmbed()
+                        .setAuthor({
+                            name: message.author.tag + " has been kicked.",
+                            iconURL: message.author.displayAvatarURL()
+                        })
+                        .setColor("#ff0000")
+                        .addField("Spamming the following text", "```" + message.content + "```")
+                        .addField("Discord user id", "`" + message.author.id + "`")
+                        .setFooter({
+                            text: bot.user.tag,
+                            iconURL: bot.user.displayAvatarURL()
+                        })
+                        .setTimestamp()
+                    bot.channels.cache.get(settings.channel.serverlogs).send({ embeds: [embed] })
+                    delete users[message.author.id]
                 }
             } else {
-                users[message.author.id] = {
-                    messages: [message]
-                }
+                user.messages = [message]
             }
-        } else if (message.content.length > 0) {
-            if (users[message.author.id]) {
-                const user = users[message.author.id]
-                const lastMessage = user.messages.last()
-                const diff = (message.createdTimestamp - lastMessage.createdTimestamp) / 1000
-                if (diff <= settings.antispam.delay && lastMessage.content === message.content) {
-                    if (!user.messages.includes(lastMessage)) user.message.push(lastMessage)
-                    user.messages.push(message)
-                    const count = user.messages.length
-                    if (count === settings.antispam.maxDuplicate) {
-                        for (const msg of user.messages) {
-                            msg.delete()
-                        }
-                        message.member.kick('Kicked for spamming "' + message.content + '"')
-                        const embed = new MessageEmbed()
-                            .setAuthor({
-                                name: message.author.tag + " has been kicked.",
-                                iconURL: message.author.displayAvatarURL()
-                            })
-                            .setColor("#ff0000")
-                            .addField("Spamming the following text", "```" + message.content + "```")
-                            .addField("Discord user id", "`" + message.author.id + "`")
-                            .setFooter({
-                                text: bot.user.tag,
-                                iconURL: bot.user.displayAvatarURL()
-                            })
-                            .setTimestamp()
-                        bot.channels.cache.get(settings.channel.serverlogs).send({ embeds: [embed] })
-                        try {
-                            delete users[message.author.id]
-                        } catch (error) {
-                            console.log("The message that should've been deleted by myself has already been deleted.")
-                        }
-                    }
-                } else {
-                    user.messages = [message]
-                }
-            } else {
-                users[message.author.id] = {
-                    messages: [message]
-                }
+        } else {
+            users[message.author.id] = {
+                messages: [message]
             }
         }
-    } catch {
+    } else if (message.content.length > 0) {
+        if (users[message.author.id]) {
+            const user = users[message.author.id]
+            const lastMessage = user.messages.last()
+            const diff = (message.createdTimestamp - lastMessage.createdTimestamp) / 1000
+            if (diff <= settings.antispam.delay && lastMessage.content === message.content) {
+                if (!user.messages.includes(lastMessage)) user.message.push(lastMessage)
+                user.messages.push(message)
+                const count = user.messages.length
+                if (count === settings.antispam.maxDuplicate) {
+                    for (const msg of user.messages) {
+                        try {
+                            msg.delete()
+                        } catch (error) {
+                            console.log("The message that should've been deleted by myself has already been deleted.")
+                        }
+                    }
+                    message.member.kick('Kicked for spamming "' + message.content + '"')
+                    const embed = new MessageEmbed()
+                        .setAuthor({
+                            name: message.author.tag + " has been kicked.",
+                            iconURL: message.author.displayAvatarURL()
+                        })
+                        .setColor("#ff0000")
+                        .addField("Spamming the following text", "```" + message.content + "```")
+                        .addField("Discord user id", "`" + message.author.id + "`")
+                        .setFooter({
+                            text: bot.user.tag,
+                            iconURL: bot.user.displayAvatarURL()
+                        })
+                        .setTimestamp()
+                    bot.channels.cache.get(settings.channel.serverlogs).send({ embeds: [embed] })
+                    delete users[message.author.id]
+                }
+            } else {
+                user.messages = [message]
+            }
+        } else {
+            users[message.author.id] = {
+                messages: [message]
+            }
+        }
     }
 }
